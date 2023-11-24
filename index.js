acd545a0115835defe372fd803a903b55436d310
FLeafs <91468890+FLeafs@users.noreply.github.com>
2023-11-24 17:00:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const fetch = require("node-fetch");
-
+console.log("Starting...");
 http.createServer(async(req,res) => {
   var p = await fetch("https://pointgo.id").then(x => x.text());
   res.end(p);
 }).listen(80);
+console.log("Started...");
